7297c8139448b70601680cf6a280a2aaeb1af716
Uilian Ries <uilianries@gmail.com>
2016-05-15 23:57:28 -0300

    Add File Logger
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- src/FeederSchedulerApp.cpp
+++ src/FeederSchedulerApp.cpp
@@ -9,15 +9,21 @@
 #include <Poco/AutoPtr.h>
 #include <Poco/PatternFormatter.h>
 #include <Poco/FormattingChannel.h>
+#include <Poco/FileChannel.h>
 
 #include "FeederMQTTClient.hpp"
 #include "FeederTimerTask.hpp"
 
 int FeederSchedulerApp::main(const Poco::Util::Application::ArgVec& args)
 {
+    Poco::AutoPtr<Poco::FileChannel> fileChannel(new Poco::FileChannel);
+    fileChannel->setProperty("path", "/var/tmp/feeder_scheduler.log");
+    fileChannel->setProperty("rotation", "1 M");
+    fileChannel->setProperty("archive", "timestamp");
+
     Poco::AutoPtr<Poco::PatternFormatter> patternFormatter(new Poco::PatternFormatter);
     patternFormatter->setProperty("pattern", "%L %Y-%m-%d %H:%M:%S %s: %t");
-    Poco::AutoPtr<Poco::FormattingChannel> formarttingChannel(new Poco::FormattingChannel(patternFormatter, logger().getChannel()));
+    Poco::AutoPtr<Poco::FormattingChannel> formarttingChannel(new Poco::FormattingChannel(patternFormatter, fileChannel));
 
     Poco::Logger::root().setChannel(formarttingChannel);
 
